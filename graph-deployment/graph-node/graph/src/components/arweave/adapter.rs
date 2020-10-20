ba63a0995180eac168ee156b73cf991415ec97fd
Gokul Alex <gokulsan@users.noreply.github.com>
2020-10-20 18:30:23 +0530

    Changes to be committed: ZK Rollups for Circom SnarkJS Verifier

--- graph-deployment/graph-node/graph/src/components/arweave/adapter.rs
+++
@@ -1,8 +0,0 @@
-use async_trait::async_trait;
-use bytes::Bytes;
-use failure::Error;
-
-#[async_trait]
-pub trait ArweaveAdapter: Send + Sync {
-    async fn tx_data(&self, tx_id: &str) -> Result<Bytes, Error>;
-}
--- graph-deployment/graph-node/server/index-node/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod request;
-mod resolver;
-mod schema;
-mod server;
-mod service;
-
-pub use self::request::IndexNodeRequest;
-pub use self::server::IndexNodeServer;
-pub use self::service::{IndexNodeService, IndexNodeServiceResponse};
