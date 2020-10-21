3baeb7f04fda7adf8a535de4bf17622fed2d138d
Gokul Alex <gokulsan@users.noreply.github.com>
2020-10-22 01:00:01 +0530

    Update and rename VerityDAO.sol to AaveProtoCampaignDAO.sol

--- aave-governance-vdf-dao/contracts/governance/AaveProtoCampaignDAO.sol
+++ aave-governance-vdf-dao/contracts/governance/AaveProtoCampaignDAO.sol
@@ -14,7 +14,7 @@ contract Beacon{
     
 }
 
-contract VerityDAO is Initializable, GSNRecipient {
+contract AaveProtoVoteAggregatorDAO is Initializable, GSNRecipient {
 
 address public BeaconContractAddress=0x79474439753C7c70011C3b00e06e559378bAD040;
 
